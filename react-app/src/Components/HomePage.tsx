11f4d2d73f564f8a13d27b1912ba9b48f64c5501
GodGoneCrazy87 <127509213+GodGoneCrazy87@users.noreply.github.com>
2024-11-11 00:15:55 +0530

    home

--- react-app/src/Components/HomePage.tsx
+++ react-app/src/Components/HomePage.tsx
@@ -18,9 +18,24 @@ const HomePage = () => {
         <Link to="/tShirtPage">
           <div><button>T-shirt Page</button></div>
         </Link>
+        <Link to="/test">
+          <div><button>test page</button></div>
+        </Link>
       </div>
     </div>
   );
 };
 
 export default HomePage;
+
+            // <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            //   <a href="#" className=" hidden whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+            //     Sign in
+            //   </a>
+            //   <a
+            //     href="#"
+            //     className="hidden ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700"
+            //   >
+            //     Sign up
+            //   </a>
+            // </div>
