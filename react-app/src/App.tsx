8ed897bbb221a36861392c5426daec766577c5bd
Om Patil <kushpo.030507@gmail.com>
2024-11-11 07:40:25 +0530

    app.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Import the components/pages
-import HomePage from './components/HomePage';
-import Coordinators from './components/Coordinators';
-import EventPage from './components/EventPage';
-import EventsGallery from './components/EventsGallery';
-import Tshirt from './components/Tshirt';
-import Test from './components/Test';
-import Navbar from './components/navbar/Navbar';
+import HomePage from "./components/HomePage";
+import Coordinators from "./components/Coordinators";
+import EventPage from "./components/EventPage";
+import EventsGallery from "./components/EventsGallery";
+import Tshirt from "./components/Tshirt";
+import Test from "./components/Test";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
